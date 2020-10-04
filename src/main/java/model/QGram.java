99aac65e4c5a0b6ea9a6164f098fcaf5ce4a5e3f
Macarena Prado Torres <maca_pradot@hotmail.com>
2020-10-04 20:27:45 -0300

    New QGram

--- src/main/java/model/QGram.java
+++ src/main/java/model/QGram.java
@@ -17,58 +17,63 @@ public class QGram {
 //        map.forEach((k, v) -> System.out.printf("%s    %d%n", k, v));
 //    }
 
-    private static Map<String, Integer> getTokens(String str) {
-        HashMap<String, Integer> map = new HashMap<>();
+    public static HashMap<String,Integer> getMap(String str){
+        // Crea un string con los # necesarios
         StringBuilder string = new StringBuilder();
         appends(string);
         string.append(str);
         appends(string);
-        //ya cree el string con los # que le falta
-        for (int i = 0; i < string.length() - (N - 1); i++) {
-            String key = makeKey(string, i);
-            if (!map.containsKey(key)) {
-                map.put(key, 1);
-            } else {
-                map.put(key, map.get(key) + 1);
+        // HashMap es mas eficiente que ArrayList
+        HashMap<String, Integer> map = new HashMap<>();
+        int count;
+        for(int i=0; i<string.length()-(N-1); i++){
+            String key = makeKey(string,i);
+            if(!map.containsKey(key)){
+                map.put(key,1);
+            }
+            else{
+                count = map.get(key);
+                map.put(key, count+1);
             }
         }
+
         return map;
     }
 
-    private static String makeKey(StringBuilder str, int index) {
+    static private String makeKey(StringBuilder str, int index){
         StringBuilder string = new StringBuilder();
-        for (int i = index; i < index + N; i++) {
+        for(int i=index; i<index+N;i++){
             string.append(str.charAt(i));
         }
         return string.toString();
     }
 
-    private static void appends(StringBuilder str) {
-        for (int i = 0; i < N - 1; i++) {
+    static private void appends(StringBuilder str){
+        for(int i=0; i<N-1; i++){
             str.append("#");
         }
     }
 
-    public static double similarity(String s1, String s2) {
-        Map<String, Integer> map1 = getTokens(s1);
-        Map<String, Integer> map2 = getTokens(s2);
-        Set<String> keys1 = map1.keySet();
-        Set<String> keys2 = map2.keySet();
-        int size1 = 0, size2 = 0, counter = 0;
-
-        for (String key : keys2) {
-            size2 += map2.get(key);
-        }
-        for (String k1 : keys1) {
-            size1 += map1.get(k1);
-            for (String k2 : keys2) {
-                if (k1.equals(k2) ) {
-                   counter+=Math.min(map1.get(k1), map2.get(k2));
-
+    static double similarity(String str1, String str2){
+        HashMap<String,Integer> map1 = getMap(str1.toUpperCase());
+        HashMap<String,Integer> map2 = getMap(str2.toUpperCase());
+        int totalKeys = 0;
+        int sharedKeys = 0;
+        for (Map.Entry<String, Integer> entry : map1.entrySet()) {
+            if(map2.containsKey(entry.getKey())){
+                int comparison = entry.getValue() - map2.get(entry.getKey());
+                if(comparison <= 0){
+                    sharedKeys += entry.getValue();
+                }
+                else{
+                    sharedKeys += map2.get(entry.getKey());
                 }
             }
-
+            totalKeys += entry.getValue();
         }
-        return (double) (size1 + size2 - (size1-counter+size2-counter)) / (size1 + size2);
+        for(Map.Entry<String, Integer> entry : map2.entrySet()){
+            totalKeys += entry.getValue();
+        }
+        return (double)(sharedKeys*2)/totalKeys;
     }
 }
