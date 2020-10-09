9c41c756008e875d37a0203821cc632cc6232e30
Micky <mconca@itba.edu.ar>
2020-10-09 10:01:17 -0300

    agrego estaciones de subte

--- src/test/java/controller/ControllerTest.java
+++ src/test/java/controller/ControllerTest.java
@@ -34,11 +34,11 @@ class ControllerTest {
 
   @Test
   void testSimilarity() {
-    Assertions.assertEquals("0,6000", String.format("%.4f", QGram.similarity("cafe p","cafe palacio")));
-    Assertions.assertEquals("0,6000", String.format("%.4f", QGram.similarity("rubi","ruby")));
-    Assertions.assertEquals("0,3077", String.format("%.4f", QGram.similarity("salesal","vale")));
-    Assertions.assertEquals("0,4286", String.format("%.4f", QGram.similarity("salesal","alale")));
-    Assertions.assertEquals("0,6667", String.format("%.4f", QGram.similarity("salesal","salale")));
+    Assertions.assertEquals("0.6000", String.format("%.4f", QGram.similarity("cafe p","cafe palacio")));
+    Assertions.assertEquals("0.6000", String.format("%.4f", QGram.similarity("rubi","ruby")));
+    Assertions.assertEquals("0.3077", String.format("%.4f", QGram.similarity("salesal","vale")));
+    Assertions.assertEquals("0.4286", String.format("%.4f", QGram.similarity("salesal","alale")));
+    Assertions.assertEquals("0.6667", String.format("%.4f", QGram.similarity("salesal","salale")));
   }
 
   @Test
